34cea204524a25d9b0b437be0b2755ba37169c25
Carlos Hurtado Comin <Carlos@client36-10-192-40.eduroam.upc.edu>
2019-10-12 19:08:16 +0200

    algo que funciona

---
+++ example.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+'''
+driver = webdriver.Chrome()
+driver.get("https://www.google.es")
+
+element = driver.find_element_by_name('q')
+element.clear()
+element.send_keys('Como ganar la HACK UPC', Keys.ENTER)
+'''
